16de81c9f54cb10dcf6ea41105e4498478a9b189
akasai <akasai>
2021-09-21 03:50:57 +0900

    doc: pod

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,8 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 1200,
-              linkImagesToOriginal: false
+              linkImagesToOriginal: false,
+              backgroundColor: `transparent`
             }
           },
           {
